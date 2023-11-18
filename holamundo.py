a4c023caec326d68c428f917e8cd8f65706b96d0
Mario Carreño Valejo <mario.carreno@iteso,mx>
2023-11-18 10:20:19 -0600

    Actualization

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,10 @@
-print("holamundo")
+class persona:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def suma(self):
+        resultado=self.a+self.b
+        return resultado
+    
+persona=persona(30,30)
+print(persona.suma())
